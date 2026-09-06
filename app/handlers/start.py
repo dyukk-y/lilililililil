569039fd6a040199d48a64447d6475e6d11c19ca
yasha <yakovdyukk@gmail.com>
2026-09-06 21:41:03 +0700

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -89,7 +89,10 @@ async def start(msg: Message, command: CommandObject, state: FSMContext):
         return
 
     await msg.answer(
-        "Привет! 👋\n\n"
+        "Привет! 👋\n"
+        "Предложи запись для размещения в канале.\n\n"
+        "⚠️ Важное правило:\n"
+        "Каждый пост должен содержать эмодзи 🧑 или 👩\n\n"
         "Выбери действие:",
         parse_mode='HTML',
         reply_markup=main_menu()
@@ -133,7 +136,10 @@ async def check_subscription_callback(cb: CallbackQuery, state: FSMContext):
 
     await cb.message.edit_text(
         "✅ <b>Отлично! Вы подписались на необходимые ресурсы</b>\n\n"
-        "Привет! 👋\n\n"
+        "Привет! 👋\n"
+        "Предложи запись для размещения в канале.\n\n"
+        "⚠️ Важное правило:\n"
+        "Каждый пост должен содержать эмодзи 🧑 или 👩\n\n"
         "Выбери действие:",
         parse_mode='HTML',
         reply_markup=main_menu()
